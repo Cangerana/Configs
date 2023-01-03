5f63c063aaa0e05eb76020d03caa05b2e63a76aa
Cangerana <mrjoaovictorc@gmail.com>
2023-01-03 00:42:37 -0300

    Added neovim configs

---
+++ .config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,19 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "help", "python", "ruby", "javascript","c", "lua", "rust" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  auto_install = true,
+
+  ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    additional_vim_regex_highlighting = false,
+  },
+}
+
