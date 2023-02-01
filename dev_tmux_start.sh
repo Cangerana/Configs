f63e7ff7f095a5c60ca96234d08b9149fc91ca24
Cangerana <mrjoaovictorc@gmail.com>
2023-02-01 16:39:03 -0300

    Improve tmux start script

--- dev_tmux_start.sh
+++ dev_tmux_start.sh
@@ -1,37 +1,40 @@
-#!/bin/sh
+#/bin/sh
 # ENVIROMENT VARIABLES:
-# NOTES_FILES 
-# CURRENT_PROJECT 
+    # NOTES_FILES 
+    # CURRENT_PROJECT 
+    # DSB
 
+# Creating the settings sessinon 
+tmux new -s settings -d
+
+tmux new-window -t settings
+tmux rename-window -t settins Distrobox
+tmux send-keys -t settings 'distrobox-enter dev' C-m
+tmux send-keys -t settings 'neofetch' C-m
+
+tmux new-window -t settings
+tmux rename-window -t settings Notes
+tmux send-keys -t settings 'nvim $NOTES_FILES' C-m
+
+tmux new-window -t settings
+tmux rename-window -t settings Firefox
+tmux send-keys -t settings 'firefox' C-m
+
+# Creating the development session
 tmux new -s dev -d
+
 tmux rename-window -t dev Docker
 tmux send-keys -t dev 'cd $CURRENT_PROJECT' C-m
 tmux send-keys -t dev 'docker compose up -d' C-m
 tmux split-window -v -t dev
-tmux send-keys -t dev 'htop' C-m
+tmux send-keys -t dev 'psql $DSB' C-m
 
 tmux new-window -t dev
 tmux rename-window -t dev Code
-tmux send-keys -t dev 'cd $CURRENT_PROJECT' C-m
-tmux send-keys -t dev 'n .' C-m
+tmux send-keys -t dev cd '$CURRENT_PROJECT' C-m
+tmux send-keys -t dev 'nvim .' C-m
 
-tmux new-window -t dev
-tmux rename-window -t dev Distrobox
-tmux send-keys -t dev 'distrobox-enter dev' C-m
-tmux send-keys -t dev 'neofetch' C-m
-tmux split-window -v -t dev
-tmux send-keys -t dev 'distrobox-enter dev' C-m
-tmux send-keys -t dev 'neofetch' C-m
-
-tmux new-window -t dev
-tmux rename-window -t dev Notes
-tmux send-keys -t dev 'nvim $NOTES_FILES' C-m
-
-tmux new-window -t dev
-tmux rename-window -t dev Firefox
-tmux send-keys -t dev 'firefox' C-m
-
-
-mux select-window -t dev:1
+tmux select-window -t dev:1
 tmux attach -t dev
+
 
